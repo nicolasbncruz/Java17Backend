ebabe978f29c55b398169d5f725c6c58cfea1efe
Nicolas Benjamin Cruz Carpio <nicolasbncruz@hotmail.com>
2022-10-30 01:15:24 -0500

    add servidor eureka

---
+++ Proyecto Final/servidor eureka/src/main/java/com/profesionaljava/examenfinal/eureka/ServidorEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.profesionaljava.examenfinal.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServidorEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorEurekaApplication.class, args);
+	}
+
+}
---
+++ Proyecto Final/servidor eureka/src/test/java/com/profesionaljava/examenfinal/eureka/ServidorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profesionaljava.examenfinal.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorEurekaApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
