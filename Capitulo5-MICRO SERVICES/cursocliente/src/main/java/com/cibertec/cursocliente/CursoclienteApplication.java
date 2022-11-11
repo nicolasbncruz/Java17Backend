5b2f614485df93e417e40736b94852023f793bb5
Nicolas Benjamin Cruz Carpio <nicolasbncruz@hotmail.com>
2022-11-10 22:52:17 -0500

    add project chapter 5

---
+++ Capitulo5-MICRO SERVICES/cursocliente/src/main/java/com/cibertec/cursocliente/CursoclienteApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.cursocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoclienteApplication.class, args);
+	}
+
+}
---
+++ Capitulo5-MICRO SERVICES/cursocliente/src/test/java/com/cibertec/cursocliente/CursoclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.cursocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
